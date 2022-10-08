77144eed920b293cee77d87fc4e09e7fe299af10
azfarmiskam <mohdazfar2401@gmail.com>
2022-10-09 00:54:37 +0800

    add BMI_calc.py and edit README.md

---
+++ BMI_Calc.py
@@ -0,0 +1,41 @@
+#! /usr/bin/env python3
+
+print("\n==============================\n")
+print("     Adult BMI Calculator     ")
+print("           by azfar")
+print("\n==============================\n\n")
+
+w = float(input("Your weight in kg: "))
+h = float(input("Your height in cm: "))
+
+def BMI(weight, height):
+    a = ((weight/height/height)*10000)
+    b = "{:.1f}".format(a)
+    print("\nYour BMI value: "+b)
+    return b
+
+def print_msg_box(msg, indent=1, width=None, title=None):
+    lines = msg.split('\n')
+    space = " " * indent
+    if not width:
+        width = max(map(len, lines))
+    box = f'╔{"═" * (width + indent * 2)}╗\n'  # upper_border
+    if title:
+        box += f'║{space}{title:<{width}}{space}║\n'  # title
+        box += f'║{space}{"-" * len(title):<{width}}{space}║\n'  # underscore
+    box += ''.join([f'║{space}{line:<{width}}{space}║\n' for line in lines])
+    box += f'╚{"═" * (width + indent * 2)}╝\n\n'  # lower_border
+    print(box)
+
+bmi = float(BMI(w, h))
+if bmi < 18.5:
+    print_msg_box('\n~ UNDERWEIGHT ~\n', indent=10)
+
+elif bmi >= 18.5 and bmi <= 24.9:
+    print_msg_box('\n~ NORMAL WEIGHT ~\n', indent=10)
+
+elif bmi >= 25 and bmi <= 29.9:
+    print_msg_box('\n~ OVERWEIGHT ~\n', indent=10)
+
+else:
+    print_msg_box('\n~ OBERSITY ~\n', indent=10)
--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # BMI_Calculator
- 
+
+Adult Body Mass Index (BMI) Calculator
+
+## BMI Categories:
+Underweight = <18.5
+Normal weight = 18.5–24.9
+Overweight = 25–29.9
+Obesity = BMI of 30 or greater
+
+## Take Action Towards Better Health:
+
+- Maintain a Healthy Weight
+- Maintaining a healthy weight is important for your heart health.
+- Increase Physical Activity
+- Moving more can lower your risk factors for heart disease.
+- Eat a Heart-Healthy Diet
+- Eating a healthy diet is the key to heart disease prevention.
